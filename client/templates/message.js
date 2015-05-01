915c602c8c7aa6f4f6910f40cddd13b2954ff76a
Teo Fleming <tfleming@student.42.fr>
2015-05-01 23:32:12 +0200

    the date now looks beautiful

--- client/templates/message.js
+++ client/templates/message.js
@@ -1,8 +1,10 @@
 Template.message.helpers({
 	dateTime: function () {
-    twentyFourHours = 24 * 60 * 60 * 1000;
-    if (this.createdAt.getTime() < Date.now() - twentyFourHours)
-      return this.createdAt.format("mmm d yyyy hh:MM");
+    twelveHours = 12 * 60 * 60 * 1000;
+    if (this.createdAt.getTime() < Date.now() - twelveHours * 2)
+      return this.createdAt.format("mmm d yyyy hh:MM TT");
+    else if (this.createdAt.getTime() < Date.now() - twelveHours)
+      return this.createdAt.format("hh:MM TT");
     else
       return this.createdAt.format("hh:MM");
   }
